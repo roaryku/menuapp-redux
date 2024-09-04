1a83ebfedcdbbf050cc3fd87dbf0ca481745a535
Rodika Aryku <raryku@yahoo.com>
2024-09-04 08:35:58 -0700

    delete button fixed

--- src/DishesApp.js
+++ src/DishesApp.js
@@ -1,5 +1,6 @@
 
 import './App.css';
+import video from './backgroundVideo.mp4'
 // import backgroundImage from './background.jpg'
 import Cart from './DessertComponent/Cart/Cart';
 import Dishes from './DessertComponent/DishesComponent/Dishes';
@@ -8,10 +9,10 @@ import AllCategories from './DessertComponent/Filter.js/AllCategories';
 function DishesApp() {
   return (
     <div className="App backgroundImage">
-        {/* <img className='backgroundImage' src={backgroundImage} alt='background'/> */}
-      {/* <video autoPlay muted loop playsInline>
+         {/* <img className='backgroundImage' src={backgroundImage} alt='background'/>  */}
+     <video autoPlay muted loop playsInline>
         <source src={video} type='video/mp4'/>
-      </video> */}
+      </video> 
 
       <div className='block cartContainer'>
           <Cart/>
--- src/FoodComponent/Cart/CartItemFood.js
+++ src/FoodComponent/Cart/CartItemFood.js
@@ -4,23 +4,23 @@ import { useDispatch } from "react-redux";
 
 const CartItemFood = ({itemsInCart}) => {
 
-    const dishes = dishesData.find(item => item.idFood === itemsInCart.dishId)
+    const recipeDishes = dishesData.find(item => item.idFood === itemsInCart.dishId)
     const dispatch = useDispatch();
-    console.log (dishes)
+    console.log (recipeDishes)
 
     return(
         <div>
         <div className="headerCart">
             <div className="container-cart"> 
-               <img className="imageInTheCart" src={`./${dishes.img}.jpg`} alt="food"/>
+               <img className="imageInTheCart" src={`./${recipeDishes.img}.jpg`} alt="food"/>
             </div>
 
         
             <div className="container-cart"> 
-               <p className="cartPar">{dishes.name}</p>
+               <p className="cartPar">{recipeDishes.name}</p>
                <p className="cartPar">{itemsInCart.quantity} portion(s)</p>
-               <p className="cartPar price">Price: ${dishes.price * itemsInCart.quantity}</p>
-               <span onClick={() => dispatch(removeItemFromCart({itemsInCartId: itemsInCart.id}))}>
+               <p className="cartPar price">Price: ${recipeDishes.price * itemsInCart.quantity}</p>
+               <span onClick={() => dispatch(removeItemFromCart({itemsInCartId: itemsInCart.idFood}))}>
                <img className="icon cartPar" src="https://img.icons8.com/material-outlined/48/000000/trash--v1.png" alt="icon"/> 
                </span>
             </div>
